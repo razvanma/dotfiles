f31900d7691cc00ffc6815af91cc8ed19bd23696
Raz Mathias <raz.mathias@gmail.com>
2012-11-03 23:03:42 +0000

    fixed install script

--- install-stuff.sh
+++ install-stuff.sh
@@ -7,15 +7,27 @@ sudo apt-get install tmux
 # Install qgit
 sudo apt-get install qgit
 
-# Download, compile and install tmux 1.6
-sudo apt-get install libevent-dev
-wget https://launchpad.net/ubuntu/+archive/primary/+files/tmux_1.6.orig.tar.gz
-tar xzfv tmux_1.6.orig.tar.gz
-cd tmux-1.6/
-./configure
-make
-sudo make install
-# move tmux over to /usr/local/bin
+# qnamebuf, search for file by name
+# README: http://www.vim.org/scripts/script.php?script_id=3217
+pushd ~/.vim
+wget -O qnamebuf.zip http://www.vim.org/scripts/download_script.php?src_id=15680
+unzip ./qnamebuf.zip
+popd
+
+# Install: vimscreen
+# README: http://www.vim.org/scripts/script.php?script_id=2711
+wget -O screen.vba http://www.vim.org/scripts/download_script.php?src_id=16100
+vim -c 'so %' -c 'q' screen.vba
+
+## Download, compile and install tmux 1.6
+#sudo apt-get install libevent-dev
+#wget https://launchpad.net/ubuntu/+archive/primary/+files/tmux_1.6.orig.tar.gz
+#tar xzfv tmux_1.6.orig.tar.gz
+#cd tmux-1.6/
+#./configure
+#make
+#sudo make install
+## Then move tmux over to /usr/local/bin
 
 ## Install R
 ## README: http://cran.r-project.org/bin/linux/ubuntu/README
@@ -23,21 +35,13 @@ sudo make install
 # sudo apt-get update
 # sudo apt-get install r-base
 
-# Install: ML for hackers packages
-# Repository: https://github.com/johnmyleswhite/ML_for_Hackers.git
-# In R: source("package_installer.R")
+## Install: ML for hackers packages
+## Repository: https://github.com/johnmyleswhite/ML_for_Hackers.git
+## In R: source("package_installer.R")
 wget https://raw.github.com/johnmyleswhite/ML_for_Hackers/master/package_installer.R
 
-# qnamebuf, search for file by name
-# http://www.vim.org/scripts/script.php?script_id=3217
-cd ~/.vim
-wget -O qnamebuf.zip http://www.vim.org/scripts/script.php?script_id=3217
-unzip qnamebuf.zip
-
-# Upgrade: to vim 7.3
-# HOWTO: http://unix.stackexchange.com/questions/5048/best-way-to-upgrade-vim-gvim-to-7-3-in-ubuntu-10-04
-
 # Install: colorout plugin for R
+
 # README: http://www.lepem.ufc.br/jaa/colorout.html
 # In R:
 #   download.file("http://www.lepem.ufc.br/jaa/colorout_1.0-0.tar.gz", destfile = "colorout_1.0-0.tar.gz")
@@ -48,13 +52,6 @@ unzip qnamebuf.zip
 # In R:
 #  download.file("http://www.lepem.ufc.br/jaa/vimcom_0.9-3.tar.gz", destfile = "vimcom_0.9-3.tar.gz")
 #  install.packages("vimcom_0.9-3.tar.gz")
-
-# Install: vimscreen
-# README: http://www.vim.org/scripts/script.php?script_id=2711
-wget -O screen.vba http://www.vim.org/scripts/download_script.php?src_id=16100
-# To install: vim screen.vba
-# :so %
-
 # vim-r
 # README: http://www.lepem.ufc.br/jaa/r-plugin.html#r-plugin-files
 
