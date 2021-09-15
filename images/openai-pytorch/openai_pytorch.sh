0a6e6e30fc0f31bebef6c40790d77544910c1a7c
Raz Mathias <raz@wide-awake.localdomain>
2021-09-15 16:04:34 -0400

    added x11 forwarding to docker setup

---
+++ images/openai-pytorch/openai_pytorch.sh
@@ -0,0 +1,11 @@
+ docker run \
+   -e DISPLAY=$DISPLAY \
+   -v /tmp/.X11-unix:/tmp/.X11-unix \
+   --gpus all \
+   --device /dev/nvidia0 \
+   --device /dev/nvidia-uvm \
+   --device /dev/nvidia-uvm-tools \
+   --device /dev/nvidiactl \
+   -e DISPLAY=unix$DISPLAY \
+   --net=host \
+    -it openai_pytorch
