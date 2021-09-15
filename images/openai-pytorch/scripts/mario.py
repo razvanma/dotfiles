538029ad3cb372edd56f42f6fac62beee2a12e40
Raz Mathias <raz@wide-awake.localdomain>
2021-09-15 18:05:40 -0400

    .

--- images/openai-pytorch/scripts/mario.py
+++ images/openai-pytorch/scripts/mario.py
@@ -1,3 +1,4 @@
+import time
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
@@ -9,6 +10,11 @@ done = True
 for step in range(5000):
     if done:
         state = env.reset()
+
+    # Facilitates reasonable x11 viz
+    # Make sure to first run 'export DISPLAY=unix:0'
+    time.sleep(0.01)
+
     state, reward, done, info = env.step(env.action_space.sample())
     env.render()
 
