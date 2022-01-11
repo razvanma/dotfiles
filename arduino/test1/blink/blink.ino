2f93faa50965bd4304725f083f07f273942381a3
Raz Mathias <raz@wide-awake.localdomain>
2022-01-11 12:43:49 -0500

    .

--- arduino/test1/blink/blink.ino
+++ arduino/test1/blink/blink.ino
@@ -1,13 +1,41 @@
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+#include <RCSwitch.h>
+
+RCSwitch s = RCSwitch();
+volatile byte state = LOW;
+
+static void handleInterrupt() {
+  state = !state;
+  digitalWrite(LED_BUILTIN, state);
 }
 
-// the loop function runs over and over again forever
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+
+  s.enableReceive(digitalPinToInterrupt(2));
+  s.enableTransmit(4);
+  s.setRepeatTransmit(10);
+  //attachInterrupt(digitalPinToInterrupt(2), handleInterrupt, CHANGE);
+
+  Serial.println("[+] Listening");
+}
+
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(200);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);                       // wait for a second
+
+  unsigned long value = 13;
+  s.send(value, 32);
+
+  if (s.available()) {
+    // digitalWrite(LED_BUILTIN, true);
+    Serial.println("Received!");
+    Serial.println(s.getReceivedValue());
+    Serial.flush();
+
+    // echo the received value
+    //s.send(value, s.getReceivedBitLength());
+
+    s.resetAvailable();
+    // digitalWrite(LED_BUILTIN, false);
+  }
+  delay(500);
 }
--- arduino/test1/blink/buildit.sh
+++ arduino/test1/blink/buildit.sh
@@ -3,14 +3,22 @@
 # Find the board name
 arduino-cli board list
 
-# Install its driver
+# Install its driver for both user and SU
 arduino-cli core install arduino:avr
-
-# Compile the sketch
-arduino-cli compile -b arduino:avr:uno ./blink.ino
-
-# Install driver so su sees the driver.
 sudo arduino-cli core install arduino:avr
 
-# SU and upload.
-sudo arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno ./blink.ino
+# Talk to the arduino
+# sudo pacman -S busybox
+sudo busybox microcom -t 9600 /dev/ttyACM0
+
+# connect to the device to send / receive serial data
+screen /dev/ttyACM0 9600
+
+# Install the RCSwitch library from github.
+# 1. Download zip from https://github.com/sui77/rc-switch 
+# 2. Run 'arduino' and go to Sketch=>Include Library=>Add Zip Library
+
+# Compile the sketch and su upload
+arduino-cli compile -b arduino:avr:uno
+sudo arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno
+
