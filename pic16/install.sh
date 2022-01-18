76b1785d427e08b661e953bf2fe824e3200d7e94
Raz Mathias <raz@wide-awake.localdomain>
2022-01-18 14:08:00 -0500

    .

---
+++ pic16/install.sh
@@ -0,0 +1,21 @@
+# Download the compiler
+sudo pacman -S sdcc
+
+# Enable /uncomment Multilib, go to /etc/pacman.conf
+
+# Update system and show 32-bit packages
+sudo pacman -Syyu
+pacman -Sl | grep lib32
+
+# Install the programmers
+yay -S lib32-fakeroot
+yay -S microchip-mplabx-bin
+
+# Run full mplab IDE, press alt=>v=>f to get screen to redraw
+mplab_ide
+
+# Run IPE
+mplab_ipe
+
+# Upload program from commandline
+java -jar /opt/microchip/mplabx/v5.50/mplab_platform/mplab_ipe/ipecmd.jar
